5370349d4d5fe431d64ee3c9f721e200edf2a34f
Elizabeth Shoots <eshoots@g.syr.edu>
2026-03-02 01:47:15 +0000

    xx

---
+++ logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-03-01T23:11:32.311568167+00:00
+-- finished_at: 2026-03-01T23:11:32.447850968+00:00
+-- elapsed: 136ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c2bf0f-0107-eefa-001d-5f470018204e
+-- desc: not provided
+select 1 as id;
